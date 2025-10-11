9c5d7693702a86b96c8adb51f6969d6eae9e9cd8
Akshaya Parida <akshayaparida2811@gmail.com>
2025-10-11 15:52:07 +0530

    feat: migrate portfolio from static HTML to Next.js with TypeScript

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="/">Akshaya Parida</a>
+      <nav>
+        <a target="_blank" href="https://github.com/akshayaparida" rel="noopener noreferrer">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/akshaya-parida-7036a426a" rel="noopener noreferrer">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a target="_blank" href="https://x.com/akshaya_parida_" rel="noopener noreferrer">
+          <i className="fa-brands fa-twitter"></i>
+        </a>
+      </nav>
+    </header>
+  );
+}
